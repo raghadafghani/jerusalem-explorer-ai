86da5b665f7a60871ff8765c344c3a0f63dc2426
raghadafghani <raghadafghani2001@gmail.com>
2026-05-04 00:08:30 +0300

    Raghad Config

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
-export default defineConfig({
-  plugins: [tanstackStart(), react(), tailwindcss(), tsConfigPaths()],
-  environments: {
-    tanstack_start_app: {
-      optimizeDeps: {
-        exclude: [
-          "@tanstack/react-start",
-          "@tanstack/react-router",
-          "@tanstack/start-server-core",
-        ],
-      },
-    },
-  },
-});
+export default defineConfig(() => ({
+  plugins: [
+    tanstackStart(),
+    nitro(),
+    react(),
+    tailwindcss(),
+    tsConfigPaths(),
+  ],
+}));
