5aec33a95fd5d8e74f75559ae4f31495ed944a1a
raghadafghani <raghadafghani@gmail.com>
2026-05-03 23:18:05 +0300

    Add: Vercel CLI

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [
     tanstackStart(),
     react(),
     tailwindcss(),
     tsConfigPaths(),
-    command === "build" ? cloudflare() : null,
   ],
 }));
